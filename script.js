c17a5acb947b6221fddb461a9f88c24f93c1ae80
Emily  Hackeling <emilyhackeling@dhcp-v880-6-00054.1xwireless.unc.edu>
2015-01-29 08:37:00 -0500

    adding files from class

---
+++ script.js
@@ -0,0 +1,24 @@
+//this file is used for learning javascript
+var userName ="Emily ";
+
+var todaysDate = new Date();
+
+
+function myFunction(){
+  document.write("hello ");
+  document.write(userName);
+  console.log(todaysDate);
+}
+
+
+
+
+document.getElementById("demo").innerHTML="this is the new info";
+document.getElementById("hello").innerHTML="this is new, by demo";
+myFunction();
+
+var myNumberVariable = 5;
+var myTextVariable = "My Variable is a string";
+var myDateVariable = new Date();
+console.log(myDateVariable);
+console.log(myNumberVariable);
